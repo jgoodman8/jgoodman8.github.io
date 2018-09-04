93147d41d3e5294daf0832a673c7890874de9b68
jgoodman8 <centoloviolino@gmail.com>
2018-09-05 01:48:51 +0200

    Fixed responsive styles - Added responsive menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,22 @@ jQuery(document).ready(function ($) {
         });
     });
 
+    var open = false;
+
     $('svg#toggle-btn').click(function () {
-        console.log('open!')
+        if (!open) {
+            $('#nav-items-mobile').addClass("open");
+            open = true;
+        } else {
+            $('#nav-items-mobile').removeClass("open");
+            open = false;
+        }
+    });
+
+    $(window).resize(function () {
+        if (open) {
+            $('#nav-items-mobile').removeClass("open");
+            open = false;
+        }
     });
 });
