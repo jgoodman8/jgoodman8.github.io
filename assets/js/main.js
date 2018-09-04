944711747f57334cd79946f1779f859ddc38f00d
kikoOne <jguzmanfd@gmail.com>
2018-09-04 05:30:36 +0200

    Updated main page with re-styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,23 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
 
     /*======= Skillset *=======*/
-    
-    
     $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
 
-        $('.level-bar-inner').each(function() {
-        
+    $(window).on('load', function () {
+
+        $('.level-bar-inner').each(function () {
+
             var itemWidth = $(this).data('level');
-            
+
             $(this).animate({
                 width: itemWidth
             }, 800);
-            
+
         });
-
     });
-   
-    
 
+    $('svg#toggle-btn').click(function () {
+        console.log('open!')
+    });
 });
